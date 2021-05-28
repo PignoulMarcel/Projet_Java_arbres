5d53fe77a6e35837b5fce124b1c78e984dd85356
Pierre Surer <pierre.surer@u-psud.fr>
2021-05-28 19:06:49 +0200

    test nouvelle branche

--- src/et3/java/projet/application/Service_esp_vert.java
+++
@@ -1,6 +0,0 @@
-package et3.java.projet.application;
-
-public class Service_esp_vert extends Mairie {
-	private String[] TenusInformes;	//Personnes ou entités voulant être informés des évènements concernant les arbres
-	
-}
