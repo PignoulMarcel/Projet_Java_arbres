b656efa25a6d0858d8efc414b02880536d718f0e
Pierre Surer <pierre.surer@u-psud.fr>
2021-05-30 19:32:49 +0200

    pitipatch

--- src/et3/java/projet/application/Association.java
+++ src/et3/java/projet/application/Association.java
@@ -2,11 +2,9 @@ package et3.java.projet.application;
 
 
 import java.time.Year;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.*;
 
 import et3.java.projet.application.Operation;
-import java.util.List;
 
 import et3.java.projet.application.Membre;
 import et3.java.projet.application.Operation;
@@ -156,16 +154,48 @@ public class Association extends Entite {
     	}
     }
 
-    public Membre ChercheMembre(Personne personne) {
-    	membres.
-    	for(int i =0; i<membres.size(); i++) {
-    		if(personne.getNom() == membres.get(i).getNom()) {
-    			if(personne.getPrenom() == membres.get(i).getPrenom()) {
-        			return membres.get(i);
-        		}
-    		}
-    	}
-    	return null;	
-    }
+	public Vote genererVotes(ArrayList<Arbre> listeArbres){
+		ArrayList<Arbre> nonRemarquables = new ArrayList<Arbre>();
+		for(Arbre arbre : listeArbres){
+			if(!arbre.getRemarquable())nonRemarquables.add(arbre);
+		}
+
+		ArrayList<Arbre> listeVotes = new ArrayList<Arbre>();
+		for(Membre membre : membres){
+			listeVotes.addAll(membre.vote(nonRemarquables).getListeArbre());
+		}
+
+		Set<Arbre> uniquesArbres = new HashSet<Arbre>(listeVotes);
+		Collections.sort((ArrayList<Arbre>)uniquesArbres, new Comparator<Arbre>() {//tres lent, a refaire
+			@Override
+			public int compare(Arbre a1, Arbre a2) {
+				boolean plusGrand=false;
+				int frqA1 = Collections.frequency(listeVotes,a1);
+				int frqA2 = Collections.frequency(listeVotes,a2);
+				if(frqA1>frqA2){
+					return 1;
+				}else if(frqA1<frqA2){
+					return 0;
+				}
+
+				if(a1.getCirconference()>a2.getCirconference()){
+					return 1;
+				}else if(a1.getCirconference()<a2.getCirconference()){
+					return 0;
+				}
+
+				if(a1.getHauteur()>a2.getHauteur()){
+					return 1;
+				}else if(a1.getHauteur()<a2.getHauteur()){
+					return 0;
+				}
+
+				return 1;
+			}
+		});
+
+		return new Vote((ArrayList<Arbre>)uniquesArbres);
+
+	}
 	
 }
